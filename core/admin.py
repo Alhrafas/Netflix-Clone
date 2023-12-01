b5fc51e365bb630db9751f64876ba5d957fda060
Alhrafas <abbadegugger@gmail.com>
2023-12-01 12:33:11 +0100

    initial commit

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import  Movies, Profile, CustomUser,Videos
 # Register your models here.
+admin.site.register(Movies)
+admin.site.register(Profile)
+admin.site.register(CustomUser)
+admin.site.register(Videos)
